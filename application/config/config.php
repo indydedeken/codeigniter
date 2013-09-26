d078e33288643c9665a314f93c31428933260aff
Indy <dev@indydedeken.fr>
2013-09-26 12:01:06 +0200

    Update base_url() value
    // pour tester sur les appareils mobiles
    $config['base_url']	= 'http://192.168.194.200/codeigniter/';
    // pour tester en local
    $config['base_url']	= 'http://localhost/codeigniter/';

--- application/config/config.php
+++ application/config/config.php
@@ -15,6 +15,7 @@
 |
 */
 $config['base_url']	= 'http://192.168.194.200/codeigniter/';
+$config['base_url']	= 'http://localhost/codeigniter/';
 
 /*
 |--------------------------------------------------------------------------
