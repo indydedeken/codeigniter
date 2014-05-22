625e27de9222140c9ce7a47a08484d329191801a
Indy <dev@indydedeken.fr>
2014-05-22 15:38:30 +0200

    [BDD] MarkUs
    màj propre de la bdd

--- markusBDD.sql
+++ markusBDD.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:8889
--- Généré le :  Ven 16 Mai 2014 à 00:41
+-- Généré le :  Jeu 22 Mai 2014 à 15:37
 -- Version du serveur :  5.5.34
 -- Version de PHP :  5.5.10
 
@@ -15,6 +15,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `markus`
 --
+CREATE DATABASE IF NOT EXISTS `markus` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `markus`;
 
 -- --------------------------------------------------------
 
@@ -22,7 +24,6 @@ SET time_zone = "+00:00";
 -- Structure de la table `Annotation`
 --
 
-DROP TABLE IF EXISTS `Annotation`;
 CREATE TABLE `Annotation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `idDocument` int(11) NOT NULL,
@@ -35,17 +36,7 @@ CREATE TABLE `Annotation` (
   `dateCreation` varchar(50) NOT NULL,
   `dateModification` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
-
---
--- Contenu de la table `Annotation`
---
-
-INSERT INTO `Annotation` (`id`, `idDocument`, `idGroupe`, `emailUtilisateur`, `contenu`, `coordX`, `coodY`, `idTypeAnnotation`, `dateCreation`, `dateModification`) VALUES
-(1, 1, 1, 'indy@indy.fr', 'test de contenu', '100', '200', 0, '27/01/2014', ''),
-(2, 1, 0, 'indy@indy.fr', 'test de contenu2', '100', '250', 0, '27/01/2014', ''),
-(3, 2, 0, 'axel@axel.fr', 'test de contenu3', '100', '270', 0, '27/01/2014', ''),
-(5, 1, 2, 'indy@indy.fr', 'test de contenu', '100', '200', 0, '27/01/2014', '');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -53,7 +44,6 @@ INSERT INTO `Annotation` (`id`, `idDocument`, `idGroupe`, `emailUtilisateur`, `c
 -- Structure de la table `Document`
 --
 
-DROP TABLE IF EXISTS `Document`;
 CREATE TABLE `Document` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `emailUtilisateur` varchar(100) NOT NULL,
@@ -65,22 +55,7 @@ CREATE TABLE `Document` (
   `dateCreation` varchar(50) NOT NULL,
   `dateModification` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=29 ;
-
---
--- Contenu de la table `Document`
---
-
-INSERT INTO `Document` (`id`, `emailUtilisateur`, `auteur`, `titre`, `description`, `contenuOriginal`, `etat`, `dateCreation`, `dateModification`) VALUES
-(1, 'indy@indy.fr', 'Axel Ajavon & Luis Braga Miguel Seixas José & Indy De Deken', 'Un titre de document peut-être très long comme celui-ci. Alors comment est-ce que l''affichage réagit ?', '', '<html><head><style>p{color:red;}</style></head><body><h1>Ma Vie</h1><p>Bonjour, c''est ma vie. Voilà le texte qui sert de test.</p></body></html>', 0, '25/01/2014', ''),
-(2, 'luis@luis.fr', 'Guy de Maupassant', 'Pierre et Jean', '', '<html><head><style>p{color:red;}</style></head><body><h1>Ma Vie</h1><p>Bonjour, c''est ma vie. Voilà le texte qui sert de test.</p></body></html>', 2, '26/01/2014', ''),
-(3, 'indy@indy.fr', 'Guy de Maupassant', 'Le horlat', '', '<html><head><style>p{color:red;}</style></head><body><h1>Ma Vie</h1><p>Bonjour, c''est ma vie. Voilà le texte qui sert de test.</p></body></html>', 0, '27/01/2014', ''),
-(4, 'indy@indy.fr', 'Pedro Porto', 'La cuisine portugaise au Moyen-Âge', 'Ce recueil de poésie datant du XXIè siècle est une ôde à la maçonnerie contractuelle.\r\nLes jambes de mon fauteuil sont en vinyl de soie.', 'contenu du pdf', 1, '30/01/2014', NULL),
-(7, 'indy@indy.fr', '', 'Calendrier Evry L3 Miage 2012 2013', '', '<html>mon contenu original bla bla</html>', 0, '13/04/2014', NULL),
-(8, 'indy@indy.fr', 'Auteur//Indy', 'Titre"Calendrier de la MIAGE', 'Description//Mon calendrier de MIAGE', '<html>mon contenu original bla bla</html>', 0, '13/04/2014', NULL),
-(9, 'indy@indy.fr', '', 'Calendrier Evry L3 Miage 2012 2013', '', '<html>mon contenu original bla bla</html>', 0, '13/04/2014', NULL),
-(25, 'indy@indy.fr', 'Indy', '[Demo] StarUML', '', '<html>mon contenu original bla bla</html>', 0, '02/05/2014', NULL),
-(28, 'indy@indy.fr', '', 'TEST', '', '<html>mon contenu original bla bla</html>', 0, '15/05/2014', NULL);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -88,7 +63,6 @@ INSERT INTO `Document` (`id`, `emailUtilisateur`, `auteur`, `titre`, `descriptio
 -- Structure de la table `EtatDocument`
 --
 
-DROP TABLE IF EXISTS `EtatDocument`;
 CREATE TABLE `EtatDocument` (
   `id` int(11) NOT NULL,
   `libelle` varchar(20) NOT NULL
@@ -109,7 +83,6 @@ INSERT INTO `EtatDocument` (`id`, `libelle`) VALUES
 -- Structure de la table `GestionAcces`
 --
 
-DROP TABLE IF EXISTS `GestionAcces`;
 CREATE TABLE `GestionAcces` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `idGroupe` int(11) NOT NULL,
@@ -119,19 +92,7 @@ CREATE TABLE `GestionAcces` (
   `dateValidation` varchar(250) DEFAULT NULL,
   `avis` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
-
---
--- Contenu de la table `GestionAcces`
---
-
-INSERT INTO `GestionAcces` (`id`, `idGroupe`, `emailAdministrateur`, `emailUtilisateur`, `dateDemande`, `dateValidation`, `avis`) VALUES
-(1, 8, 'luis@luis.fr', 'indy@indy.fr', '15/05/2014', NULL, 2),
-(2, 1, 'indy@indy.fr', 'luis@luis.fr', '15/05/2014', NULL, 0),
-(3, 1, 'indy@indy.fr', 'axel@axel.fr', '15/05/2014', NULL, 0),
-(4, 2, 'indy@indy.fr', 'luis@luis.fr', '15/05/2014', NULL, 0),
-(5, 3, 'indy@indy.fr', 'luis@luis.fr', '15/05/2014', NULL, 0),
-(7, 1, 'indy@indy.fr', 'jean.gui@gmail.com', '16/05/2014', NULL, 0);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -139,7 +100,6 @@ INSERT INTO `GestionAcces` (`id`, `idGroupe`, `emailAdministrateur`, `emailUtili
 -- Structure de la table `Groupe`
 --
 
-DROP TABLE IF EXISTS `Groupe`;
 CREATE TABLE `Groupe` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `intitule` varchar(255) NOT NULL,
@@ -147,7 +107,7 @@ CREATE TABLE `Groupe` (
   `dateCreation` varchar(50) NOT NULL,
   `emailAdministrateur` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Contenu de la table `Groupe`
@@ -155,19 +115,9 @@ CREATE TABLE `Groupe` (
 
 INSERT INTO `Groupe` (`id`, `intitule`, `description`, `dateCreation`, `emailAdministrateur`) VALUES
 (0, 'Bibliothèque', 'Bibliothèque personnelles', '16/04/2014', ''),
-(1, 'Faculté d''Évry', 'Ce groupe est un rassemblement de professeurs issus de l''université d''Évry', '25/01/2014', 'indy@indy.fr'),
+(1, 'Faculté d''Évry', 'Ce groupe est un rassemblement de professeurs issus de l''université d''Évry', '25/01/2014', 'luis@luis.fr'),
 (2, 'MIAGE Université d''Évry Val d''Essonne', 'Groupement des élèves de toutes les promotions MIAGE.', '25/01/2014', 'indy@indy.fr'),
-(3, 'hello GRP', 'description ', '30/01/2014', 'indy@indy.fr'),
-(6, 'Test3', 'Description 3', '30/01/2014', 'indy@indy.fr'),
-(7, 'Test4', 'Description 4', '30/01/2014', 'indy@indy.fr'),
-(8, 'Groupe de Luis Moustache', 'Mon crew de lecture est ici.', '30/01/2014', 'luis@luis.fr'),
-(9, 'Groupe de Indy', 'Bienvenue dans le groupe', '31/01/2014', 'indy@indy.fr'),
-(10, 'Groupe de Joséba', 'Hello Josh', '31/01/2014', 'indy@indy.fr'),
-(11, 'Je suis pas ton pote mon gars', 'A bon ?', '31/01/2014', 'indy@indy.fr'),
-(12, 'Le groupe des Nachos', 'On aime les Nachos', '31/01/2014', 'luis@luis.fr'),
-(14, 'Bonjour  Groupe', 'Bonjour', '31/01/2014', 'indy@indy.fr'),
-(15, 'Au revoir Groupe', 'Groupe', '31/01/2014', 'indy@indy.fr'),
-(16, 'Mon énième groupe', '', '06/02/2014', 'indy@indy.fr');
+(3, 'Groupe de recherche', 'description', '30/01/2014', 'axel@axel.fr');
 
 -- --------------------------------------------------------
 
@@ -175,7 +125,6 @@ INSERT INTO `Groupe` (`id`, `intitule`, `description`, `dateCreation`, `emailAdm
 -- Structure de la table `GroupeDocument`
 --
 
-DROP TABLE IF EXISTS `GroupeDocument`;
 CREATE TABLE `GroupeDocument` (
   `idGroupe` int(11) NOT NULL,
   `idDocument` int(11) NOT NULL,
@@ -183,37 +132,12 @@ CREATE TABLE `GroupeDocument` (
   PRIMARY KEY (`idGroupe`,`idDocument`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Contenu de la table `GroupeDocument`
---
-
-INSERT INTO `GroupeDocument` (`idGroupe`, `idDocument`, `contenu`) VALUES
-(0, 1, ''),
-(0, 2, ''),
-(0, 3, ''),
-(0, 4, ''),
-(0, 7, ''),
-(0, 8, ''),
-(0, 9, ''),
-(0, 25, ''),
-(0, 28, ''),
-(1, 1, ''),
-(1, 2, ''),
-(1, 4, ''),
-(2, 3, ''),
-(3, 4, ''),
-(6, 1, ''),
-(7, 1, ''),
-(7, 3, ''),
-(8, 2, '');
-
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `GroupeUtilisateur`
 --
 
-DROP TABLE IF EXISTS `GroupeUtilisateur`;
 CREATE TABLE `GroupeUtilisateur` (
   `idGroupe` int(11) NOT NULL,
   `emailUtilisateur` varchar(100) NOT NULL,
@@ -226,15 +150,9 @@ CREATE TABLE `GroupeUtilisateur` (
 --
 
 INSERT INTO `GroupeUtilisateur` (`idGroupe`, `emailUtilisateur`, `dateInscriptionGroupe`) VALUES
-(1, 'axel@axel.fr', ''),
-(1, 'indy@indy.fr', '25/01/2014'),
-(2, 'indy@indy.fr', '25/01/2014'),
-(3, 'indy@indy.fr', '12/04/2014'),
-(6, 'indy@indy.fr', '30/01/2014'),
-(7, 'indy@indy.fr', '30/01/2014'),
-(8, 'luis@luis.fr', ''),
-(12, 'luis@luis.fr', '15/05/2014'),
-(16, 'indy@indy.fr', '06/02/2014');
+(1, 'luis@luis.fr', '15/05/2014'),
+(2, 'indy@indy.fr', '15/05/2014'),
+(3, 'axel@axel.fr', '15/05/2014');
 
 -- --------------------------------------------------------
 
@@ -242,7 +160,6 @@ INSERT INTO `GroupeUtilisateur` (`idGroupe`, `emailUtilisateur`, `dateInscriptio
 -- Structure de la table `TypeAnnotation`
 --
 
-DROP TABLE IF EXISTS `TypeAnnotation`;
 CREATE TABLE `TypeAnnotation` (
   `id` int(11) NOT NULL,
   `libelle` varchar(50) NOT NULL,
@@ -264,7 +181,6 @@ INSERT INTO `TypeAnnotation` (`id`, `libelle`) VALUES
 -- Structure de la table `Utilisateur`
 --
 
-DROP TABLE IF EXISTS `Utilisateur`;
 CREATE TABLE `Utilisateur` (
   `email` varchar(100) NOT NULL,
   `nom` varchar(50) NOT NULL,
