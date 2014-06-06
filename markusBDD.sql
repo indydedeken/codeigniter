5b17ae723afe8ae311b61a442181c8daf88fb85f
BragaLuis <Seixas.luis.miguel@gmail.com>
2014-06-06 11:15:53 +0200

    [BDD] MarkUs
    Base de données propre avec les images/fichiers (+pdf) générés par
    l'upload.

--- markusBDD.sql
+++ markusBDD.sql
@@ -1,27 +1,28 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.9
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
--- Client :  localhost:8889
--- Généré le :  Ven 23 Mai 2014 à 18:13
--- Version du serveur :  5.5.34
--- Version de PHP :  5.5.10
+-- Client: localhost
+-- Généré le: Ven 06 Juin 2014 à 09:13
+-- Version du serveur: 5.6.12-log
+-- Version de PHP: 5.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Base de données :  `markus`
+-- Base de données: `markus`
 --
+CREATE DATABASE IF NOT EXISTS `markus` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `markus`;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Annotation`
+-- Structure de la table `annotation`
 --
 
-DROP TABLE IF EXISTS `Annotation`;
-CREATE TABLE `Annotation` (
+CREATE TABLE IF NOT EXISTS `annotation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `idDocument` int(11) NOT NULL,
   `idGroupe` int(11) NOT NULL,
@@ -35,14 +36,18 @@ CREATE TABLE `Annotation` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Vider la table avant d'insérer `annotation`
+--
+
+TRUNCATE TABLE `annotation`;
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Document`
+-- Structure de la table `document`
 --
 
-DROP TABLE IF EXISTS `Document`;
-CREATE TABLE `Document` (
+CREATE TABLE IF NOT EXISTS `document` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `emailUtilisateur` varchar(100) NOT NULL,
   `auteur` varchar(100) NOT NULL,
@@ -53,33 +58,42 @@ CREATE TABLE `Document` (
   `dateCreation` varchar(50) NOT NULL,
   `dateModification` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
--- Contenu de la table `Document`
+-- Vider la table avant d'insérer `document`
 --
 
-INSERT INTO `Document` (`id`, `emailUtilisateur`, `auteur`, `titre`, `description`, `contenuOriginal`, `etat`, `dateCreation`, `dateModification`) VALUES
-(1, 'indy@indy.fr', 'Indy De Deken', 'TD Crypto', '', '<html>mon contenu original bla bla</html>', 0, '23/05/2014', NULL),
-(2, 'indy@indy.fr', 'Indy', 'Extrait du diagramme de classe', '', '<html>mon contenu original bla bla</html>', 0, '23/05/2014', NULL);
+TRUNCATE TABLE `document`;
+--
+-- Contenu de la table `document`
+--
+
+INSERT INTO `document` (`id`, `emailUtilisateur`, `auteur`, `titre`, `description`, `contenuOriginal`, `etat`, `dateCreation`, `dateModification`) VALUES
+(2, 'luis@luis.fr', 'Didier C.', 'Cours de BADA numero 1', 'Premier cours de BADA en M1 Miage', './filesUploaded/user_6b2710860069ce7e18a540219b459d5c/CoursBADA_1-html.html', 0, '06/06/2014', NULL),
+(3, 'indy@indy.fr', 'Axel A.', 'Extrait d''examen AD', 'Extrait de 2011-2012', './filesUploaded/user_a5e673e5d6dfd2aed96f51881037fe89/ExtraitExamenAD_M1_2011-2012-html.html', 0, '06/06/2014', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `EtatDocument`
+-- Structure de la table `etatdocument`
 --
 
-DROP TABLE IF EXISTS `EtatDocument`;
-CREATE TABLE `EtatDocument` (
+CREATE TABLE IF NOT EXISTS `etatdocument` (
   `id` int(11) NOT NULL,
   `libelle` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `EtatDocument`
+-- Vider la table avant d'insérer `etatdocument`
 --
 
-INSERT INTO `EtatDocument` (`id`, `libelle`) VALUES
+TRUNCATE TABLE `etatdocument`;
+--
+-- Contenu de la table `etatdocument`
+--
+
+INSERT INTO `etatdocument` (`id`, `libelle`) VALUES
 (0, 'Ouvert'),
 (1, 'Publié'),
 (2, 'Terminé');
@@ -87,11 +101,10 @@ INSERT INTO `EtatDocument` (`id`, `libelle`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `GestionAcces`
+-- Structure de la table `gestionacces`
 --
 
-DROP TABLE IF EXISTS `GestionAcces`;
-CREATE TABLE `GestionAcces` (
+CREATE TABLE IF NOT EXISTS `gestionacces` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `idGroupe` int(11) NOT NULL,
   `emailAdministrateur` varchar(250) NOT NULL,
@@ -100,23 +113,28 @@ CREATE TABLE `GestionAcces` (
   `dateValidation` varchar(250) DEFAULT NULL,
   `avis` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
--- Contenu de la table `GestionAcces`
+-- Vider la table avant d'insérer `gestionacces`
 --
 
-INSERT INTO `GestionAcces` (`id`, `idGroupe`, `emailAdministrateur`, `emailUtilisateur`, `dateDemande`, `dateValidation`, `avis`) VALUES
-(4, 3, 'axel@axel.fr', 'indy@indy.fr', '22/05/2014', NULL, 0);
+TRUNCATE TABLE `gestionacces`;
+--
+-- Contenu de la table `gestionacces`
+--
+
+INSERT INTO `gestionacces` (`id`, `idGroupe`, `emailAdministrateur`, `emailUtilisateur`, `dateDemande`, `dateValidation`, `avis`) VALUES
+(4, 3, 'axel@axel.fr', 'indy@indy.fr', '22/05/2014', NULL, 0),
+(5, 1, 'luis@luis.fr', 'indy@indy.fr', '06/06/2014', '06/06/2014', 1);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Groupe`
+-- Structure de la table `groupe`
 --
 
-DROP TABLE IF EXISTS `Groupe`;
-CREATE TABLE `Groupe` (
+CREATE TABLE IF NOT EXISTS `groupe` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `intitule` varchar(255) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
@@ -126,10 +144,15 @@ CREATE TABLE `Groupe` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
--- Contenu de la table `Groupe`
+-- Vider la table avant d'insérer `groupe`
 --
 
-INSERT INTO `Groupe` (`id`, `intitule`, `description`, `dateCreation`, `emailAdministrateur`) VALUES
+TRUNCATE TABLE `groupe`;
+--
+-- Contenu de la table `groupe`
+--
+
+INSERT INTO `groupe` (`id`, `intitule`, `description`, `dateCreation`, `emailAdministrateur`) VALUES
 (0, 'Bibliothèque', 'Bibliothèque personnelles', '16/04/2014', ''),
 (1, 'Faculté d''Évry', 'Ce groupe est un rassemblement de professeurs issus de l''université d''Évry', '25/01/2014', 'luis@luis.fr'),
 (2, 'MIAGE Université d''Évry Val d''Essonne', 'Groupement des élèves de toutes les promotions MIAGE.', '25/01/2014', 'indy@indy.fr'),
@@ -138,11 +161,10 @@ INSERT INTO `Groupe` (`id`, `intitule`, `description`, `dateCreation`, `emailAdm
 -- --------------------------------------------------------
 
 --
--- Structure de la table `GroupeDocument`
+-- Structure de la table `groupedocument`
 --
 
-DROP TABLE IF EXISTS `GroupeDocument`;
-CREATE TABLE `GroupeDocument` (
+CREATE TABLE IF NOT EXISTS `groupedocument` (
   `idGroupe` int(11) NOT NULL,
   `idDocument` int(11) NOT NULL,
   `contenu` longtext NOT NULL,
@@ -150,23 +172,26 @@ CREATE TABLE `GroupeDocument` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `GroupeDocument`
+-- Vider la table avant d'insérer `groupedocument`
 --
 
-INSERT INTO `GroupeDocument` (`idGroupe`, `idDocument`, `contenu`) VALUES
-(0, 1, ''),
+TRUNCATE TABLE `groupedocument`;
+--
+-- Contenu de la table `groupedocument`
+--
+
+INSERT INTO `groupedocument` (`idGroupe`, `idDocument`, `contenu`) VALUES
 (0, 2, ''),
-(2, 1, ''),
-(2, 2, '');
+(0, 3, ''),
+(1, 2, '');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `GroupeUtilisateur`
+-- Structure de la table `groupeutilisateur`
 --
 
-DROP TABLE IF EXISTS `GroupeUtilisateur`;
-CREATE TABLE `GroupeUtilisateur` (
+CREATE TABLE IF NOT EXISTS `groupeutilisateur` (
   `idGroupe` int(11) NOT NULL,
   `emailUtilisateur` varchar(100) NOT NULL,
   `dateInscriptionGroupe` varchar(50) NOT NULL,
@@ -174,10 +199,16 @@ CREATE TABLE `GroupeUtilisateur` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `GroupeUtilisateur`
+-- Vider la table avant d'insérer `groupeutilisateur`
 --
 
-INSERT INTO `GroupeUtilisateur` (`idGroupe`, `emailUtilisateur`, `dateInscriptionGroupe`) VALUES
+TRUNCATE TABLE `groupeutilisateur`;
+--
+-- Contenu de la table `groupeutilisateur`
+--
+
+INSERT INTO `groupeutilisateur` (`idGroupe`, `emailUtilisateur`, `dateInscriptionGroupe`) VALUES
+(1, 'indy@indy.fr', '06/06/2014'),
 (1, 'luis@luis.fr', '15/05/2014'),
 (2, 'indy@indy.fr', '15/05/2014'),
 (2, 'luis@luis.fr', '23/05/2014'),
@@ -186,11 +217,10 @@ INSERT INTO `GroupeUtilisateur` (`idGroupe`, `emailUtilisateur`, `dateInscriptio
 -- --------------------------------------------------------
 
 --
--- Structure de la table `TypeAnnotation`
+-- Structure de la table `typeannotation`
 --
 
-DROP TABLE IF EXISTS `TypeAnnotation`;
-CREATE TABLE `TypeAnnotation` (
+CREATE TABLE IF NOT EXISTS `typeannotation` (
   `id` int(11) NOT NULL,
   `libelle` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
@@ -198,21 +228,25 @@ CREATE TABLE `TypeAnnotation` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `TypeAnnotation`
+-- Vider la table avant d'insérer `typeannotation`
 --
 
-INSERT INTO `TypeAnnotation` (`id`, `libelle`) VALUES
+TRUNCATE TABLE `typeannotation`;
+--
+-- Contenu de la table `typeannotation`
+--
+
+INSERT INTO `typeannotation` (`id`, `libelle`) VALUES
 (1, 'commentaire'),
 (2, 'surlignage');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Utilisateur`
+-- Structure de la table `utilisateur`
 --
 
-DROP TABLE IF EXISTS `Utilisateur`;
-CREATE TABLE `Utilisateur` (
+CREATE TABLE IF NOT EXISTS `utilisateur` (
   `email` varchar(100) NOT NULL,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
@@ -222,10 +256,15 @@ CREATE TABLE `Utilisateur` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `Utilisateur`
+-- Vider la table avant d'insérer `utilisateur`
 --
 
-INSERT INTO `Utilisateur` (`email`, `nom`, `prenom`, `mdp`) VALUES
+TRUNCATE TABLE `utilisateur`;
+--
+-- Contenu de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`email`, `nom`, `prenom`, `mdp`) VALUES
 ('axel@axel.fr', 'axel', 'axel', 'axel'),
 ('cart@man.us', 'Le mécano', 'Paulo', 'cartman'),
 ('indy@indy.fr', 'De Deken', 'Indy', 'indy'),
