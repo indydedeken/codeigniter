26401bf836518e6f0ffb556c79b8fd10fd4f4b7a
Indy <dev@indydedeken.fr>
2014-06-18 16:09:44 +0200

    [Groupe] Gif pendant la création
    Image de loading pendant la création d’un groupe.

--- application/views/groupe/vue_creer_groupe_succes.php
+++ application/views/groupe/vue_creer_groupe_succes.php
@@ -1,72 +1,23 @@
-<div class="container"> <br>
-  <br>
-  <div class="annonce">
-    <div class="col col-sm-4 col-md-3">
-      <h1>Création de groupe :</h1>
+<div class="container">
+	<br>
+	<br>
+	<div class="annonce">
+        <div style="background:#8AB7A0;padding-top:20px;font-weight:400;font-size:26px;text-align:center;" class="">
+        	<p style="color:#FFFFFF">
+                Le groupe "Test" a été créé !
+            </p>
+            <img alt="Patience..." src="http://markus.compri.me/asset/img/wait.gif" id="image">
+        </div>
     </div>
-    <div class="justify col-sm-8 col-md-9">
-      <div class="">
-		Le groupe "<?=$nom?>" est créé.<br>
-		Sa description est : "<?=$description?>".<br>
-		Vous allez être redirigé vers la page du groupe dans un instant.
-      </div>
-    </div>
-  </div>
 </div>
 <script>
-$(function() {
-	var direction = 'window.location.replace("<?php echo base_url('groupe/afficher').'/'.$idGroupe;?>");';
-	setTimeout(direction, 4000); 
-});
-</script> 
-<script type="application/javascript"><!--
-	$(document).ready(function() {
+	$(function() {
+		
+		$(window).resize(function() {
+			$('#image').css('width', $(window).width()-200);
+		});
+
+		var direction = 'window.location.replace("<?php echo base_url('groupe/afficher').'/'.$idGroupe;?>");';
+		setTimeout(direction, 3000); 
 	});
---></script> 
-<script type="application/javascript">
-	/*
-	 * Préparation des boites de notification
-	 * generateAlert()
-	 * generateSuccess()
-	 * generateError()
-	 */
-	 
-	//
-	function generateAlert(msg) {
-		var n = noty({
-			text        : msg,
-			type        : 'alert',
-			dismissQueue: true,
-			layout      : 'topCenter',
-			theme       : 'defaultTheme',
-			closeWith	: ['click'],
-			maxVisible	: 3,
-			timeout		: 10000
-		});
-	}
-	function generateSuccess(msg) {
-		var n = noty({
-			text        : msg,
-			type        : 'success',
-			dismissQueue: true,
-			layout      : 'topCenter',
-			theme       : 'defaultTheme',
-			closeWith	: ['click'],
-			maxVisible	: 3,
-			timeout		: 3000
-		});
-	}
-	//
-	function generateError(msg) {
-		var n = noty({
-			text        : msg,
-			type        : 'warning',
-			dismissQueue: true,
-			layout      : 'topCenter',
-			theme       : 'defaultTheme',
-			closeWith	: ['click'],
-			maxVisible	: 3,
-			timeout		: false
-		});
-	}
 </script>
