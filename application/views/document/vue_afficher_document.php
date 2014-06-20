30d452de186bd9e3d831362d24675e72a9dd1cbc
BragaLuis <Seixas.luis.miguel@gmail.com>
2014-06-20 13:18:30 +0200

    Fonction surligner
    Permet de surligner du texte dans le PDF.
    on peut choisir la couleur du surlignage parmit une palette de couleur.
    pour l'instant le surlignage n'est pas enregistrer.

--- application/views/document/vue_afficher_document.php
+++ application/views/document/vue_afficher_document.php
@@ -103,17 +103,12 @@
 		<div id="panel">
 			<div id="comment"> <img style="height:40px;" src="<?=base_url()?>asset/img/comment.png"> </div>
 			<div id="pencil"> <img style="height:40px;" src="<?=base_url()?>asset/img/pencil.png"> </div>
-			<div id="surligner"> <img style="height:40px;" src="<?=base_url()?>asset/img/surligner.png"> </div>
-				<!--
-				<div id="cBlack" style="background-color:black;"> </div>
-				<div id="cRed" style="background-color:red;"> </div>
-				<div id="cBlue" style="background-color:blue;"> </div>
-				<div id="cGreen" style="background-color:green;"> </div>
-				-->
+			<div class="imagebutton" id="hilitecolor"><img class="image" src="<?=base_url()?>asset/img/surligner.png" ></div>
+			<iframe width="250" height="170" id="colorpalette" src="<?=base_url()?>/colors.html" style="visibility:hidden; position: absolute;"></iframe>
 		</div>
-      	<div class="bloc_groupe">
-      		<!-- affichage des informations du document -->
-            <iframe src="<?=base_url().$doc->contenuOriginal?>" height="850px" width="100%"> </iframe>
+      	<div class="bloc_groupe"
+		<!-- affichage des informations du document -->
+			<iframe id="edit" src="<?=base_url().$doc->contenuOriginal?>" height="850px" width="100%"> </iframe>	
             <dl class="dl-horizontal">
 			</dl>
         </div>
@@ -123,6 +118,37 @@
             </div>
 	</div>
 </div>
+
+<script>
+var command = "";
+
+function Start() {
+	//passe l'iframe en modifiable
+	//effet secondaire : sous firefox quanbd on clic sur du texte dans l'iframe, on peut les contours, le deplacer et l'agrandir
+	document.getElementById('edit').contentWindow.document.designMode = "on";
+	
+	var hilite = document.getElementById('hilitecolor');
+	parent.command = hilite.id;
+  
+	//affiche ou cache la palette de couleur quand on click sur le bontton pour surligner
+	var clicHilite = false;
+	$("#hilitecolor").click(function(e) {
+		if(clicHilite === false)
+		{
+			document.getElementById("colorpalette").style.visibility="visible";
+			clicHilite = true;
+			console.log(clicHilite);
+		} else {
+			document.getElementById("colorpalette").style.visibility="hidden";
+			clicHilite = false;
+			console.log(clicHilite);
+		}
+	});
+}
+// charge la fonction start() au chargement de la page
+window.onload=Start;
+</script>	
+
 <script type="application/javascript">
 	/*
 	* afficher panel lorsqu'on clic sur #toolBox
@@ -146,6 +172,231 @@
 	$('#toolBox').click(function() {
 		$('#panel').toggle("slow");
 	});
+	
+	
+	/*
+	*	JS Annotation
+	* recupere les information de l'élément où on clic
+	*/
+
+/* attempt to find a text selection */
+function getSelected() {
+	if(window.getSelection) { return window.getSelection(); }
+	else if(document.getSelection) { return document.getSelection(); }
+	else {
+		var selection = document.selection && document.selection.createRange();
+		if(selection.text) { return selection.text; }
+		return false;
+	}
+	return false;
+}
+/* create sniffer */
+
+$(document).ready(function() {
+	$('#autresDocuments').mouseup(function() {
+		var selection = getSelected();
+		if(selection && (selection = new String(selection).replace(/^\s+|\s+$/g,''))) {
+			console.log( getSelected());
+			//console.log(selection);
+		}
+	});
+});
+/*
+var iframe = document.getElementById("iFrameTest");
+var win = iframe.contentWindow;
+var doc = win.document;
+console.log(win);
+console.log(doc.contentDocument);
+console.log(iframe.contentDocument);
+
+var x = document.getElementById("iFrameTest");
+var y = (x.contentWindow || x.contentDocument);
+console.log($(x).contents());
+
+$('#iFrameTest').load(function(){
+	console.log($(this).contents());
+});
+
+$(document).ready(function() {
+	$('#iFrameTest').load(function(){
+
+		console.log($(this).contents());
+
+			$(this).contents().find("body").mouseup(function() {
+				var selection = getSelected();
+				//if(selection && (selection = new String(selection).replace(/^\s+|\s+$/g,''))) {
+					console.log("toto");
+					console.log( getSelected());
+				//}
+			});
+	});
+});
+*/
+
+/*
+var iframe = document.getElementById("iFrameTest");
+var win = iframe.contentWindow;
+var doc = win.document;
+console.log(win);
+console.log(doc.contentDocument);
+console.log(iframe.contentDocument);
+$('#iFrameTest').load(function(){
+	console.log($(this).contents());
+});*/
+/*
+var iframe= document.getElementById('iFrameTest');
+var idoc= iframe.contentDocument || iframe.contentWindow.document; // For IE.
+console.log(idoc.getSelection());*/
+/*
+$(doc).ready(function() {
+	$(this).mouseup(function() {
+		var selection = getSelected();
+		//if(selection && (selection = new String(selection).replace(/^\s+|\s+$/g,''))) {
+			
+			console.log(selection);
+		//}
+	});
+});*/
+
+/*
+var iframe = document.getElementById("iFrameTest");
+var win = iframe.contentWindow;
+var doc = win.document;
+
+*/
+/*
+$('#iFrameTest').load(function(){
+	console.log($(this).contents());
+});
+*/
+
+/*
+(function() {
+    function getSelectedText(frameId) {
+        // In ExtJS use:
+        // var frame = Ext.getDom(frameId);
+        var frame = document.getElementById(frameId);
+
+        var frameWindow = frame && frame.contentWindow;
+        var frameDocument = frameWindow && frameWindow.document;
+
+        if (frameDocument) {
+            if (frameDocument.getSelection) {
+                // Most browsers
+                return String(frameDocument.getSelection());
+            }
+            else if (frameDocument.selection) {
+                // Internet Explorer 8 and below
+                return frameDocument.selection.createRange().text;
+            }
+            else if (frameWindow.getSelection) {
+                // Safari 3
+                return String(frameWindow.getSelection());
+            }
+	}
+        return '';
+    }
+*/
+/*
+    var interval = setInterval(function() {
+        var iframe = document.getElementById('iFrameTest');
+        var innerDocument = iframe && iframe.contentWindow && iframe.contentWindow.document;
+
+        if (innerDocument) {
+            clearInterval(interval);
+
+            setInterval(function() {
+                
+				var t = getSelectedText('iFrameTest');
+				//t.innerHTML = '<span style="color:red;">'+ t +'</span>';
+				console.log('<span style="color:red;">'+ t +'</span>');
+				console.log(t);
+            }, 500);
+        }
+    }, 500);
+})();
+*/
+
+function getSelectedText(frameId) {
+        // In ExtJS use:
+        // var frame = Ext.getDom(frameId);
+        var frame = document.getElementById(frameId);
+
+        var frameWindow = frame && frame.contentWindow;
+        var frameDocument = frameWindow && frameWindow.document;
+
+        if (frameDocument) {
+            if (frameDocument.getSelection) {
+                // Most browsers
+                //return String(frameDocument.getSelection());
+				return frameDocument.getSelection();
+            }
+            else if (frameDocument.selection) {
+                // Internet Explorer 8 and below
+                return frameDocument.selection.createRange().text;
+            }
+            else if (frameWindow.getSelection) {
+                // Safari 3
+                return String(frameWindow.getSelection());
+            }
+	}
+        return '';
+    }	
+	
+function affichage(){
+var sel = getSelectedText('iFrameTest');
+//sel.innerHTML.replace(sel, '<span style="background-color:yellow;">'+ sel +'</span>');
+//sel.replace('n', '<span style="color:red;">'+ sel +'</span>');//= '<span style="color:red;">'+ sel +'</span>';
+//sel.replaceWith('<span style="color:red;">'+ sel +'</span>');
+//sel.text = '<span style="color:red;">'+ + sel + '</span>';
+//Insert_Tag('T_MESS', "["+ szTag +"]", "[/"+ szTag +"]");
+//frameDocument.sel.execCommand("Underline");
+var regxp = new RegExp(sel, "gi");
+sel.innerHTML.replace(regex, function(matched) {
+            return "<span class=>" + matched + "</span>";
+        });
+//str = str.replace(regexp, '<span style="background-color:yellow;">'+ regexp +'</span>');
+console.log(sel);
+}	
+	
+	
+	$('iframe').load(function(){
+		$(this).contents().find("body").click(function(e) { 
+		
+			
+			//console.log(this);
+			//console.log(this.offsetHeight);
+			//console.log(this.offsetWidth);
+			//console.log(e.clientX);
+			//console.log(e.clientY);
+			
+			/*var noeud = this;
+			var fils = noeud.childNodes;
+			var nbFils = fils.length;
+			*/
+			
+			var el= e.target||event.srcElement;
+            //console.log(el);
+			//console.log(el.textContent);
+			
+			//console.log(el.getSelection);
+		
+			
+		});		
+	});
+	
+
+	/*
+	*	recupere les position de la sourie au clic 
+	*/
+	/*$('iframe').load(function(){
+		$(this).contents().find("body").click(function(e) {
+		console.log();
+			
+		});		
+	});*/
+/*	Fin JS Annotation	*/	
+	
 	
 	<!-- AJAX - changer etat document
 	$('#btnEtat').click(function() {
